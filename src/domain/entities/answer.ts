ae11da10138b8a0336ce4f26d3d7b6e487f26ae6
Tiago <tiagoufcg@gmail.com>
2024-03-27 16:55:50 -0300

    Fundamentos de DDD - Mapeando propriedades

--- src/domain/entities/answer.ts
+++ src/domain/entities/answer.ts
@@ -1,10 +1,13 @@
 import { randomUUID } from "crypto"
 import { Entity } from "../../core/entities/entity"
+import { UniqueEntityID } from "../../core/entities/unique-id-entity"
 
 interface AnswerProps{
+    authorId: UniqueEntityID
+    questionId: UniqueEntityID
     content: string  
-    authorId: string
-    questionId: string
+    createdAt: Date
+    updatedAt?: Date
 }
 
 export class Answer extends Entity<AnswerProps>{
@@ -12,9 +15,4 @@ export class Answer extends Entity<AnswerProps>{
     get content(){
         return this.props.content
     }
-    
-    constructor (props: AnswerProps, id?: string){
-        super(props, id)
-    }
-    
 }
--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,18 +1,20 @@
 import { randomUUID } from "crypto"
 import { Slug } from "./value-objects/slug"
 import { Entity } from "../../core/entities/entity"
+import { UniqueEntityID } from "../../core/entities/unique-id-entity"
 
 interface QuestionProps{
+    authorId: UniqueEntityID
+    bestAnswerId?: UniqueEntityID
     title: string
     content: string
     slug: Slug
-    authorId: string
+    createdAt: Date
+    updatedAt?: Date
 }
 
 export class Question extends Entity<QuestionProps>{
     
-    constructor (props: QuestionProps, id?:string){
-        super(props, id)
-    }  
+
    
 }
