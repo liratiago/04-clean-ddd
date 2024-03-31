553d32baddbae027e0e6eda465edd2eb6a97e37a
Tiago <tiagoufcg@gmail.com>
2024-03-31 15:48:21 -0300

    usando o faker

--- test/factories/make-question.ts
+++ test/factories/make-question.ts
@@ -4,15 +4,21 @@ import {
   QuestionProps,
 } from '@/domain/forum/enterprise/entities/question'
 import { Slug } from '@/domain/forum/enterprise/entities/value-objects/slug'
+import { faker } from '@faker-js/faker'
 
-export function makeQuestion(override: Partial<QuestionProps> = {}) {
-  const question = Question.create({
-    authorId: new UniqueEntityID(),
-    title: 'Example Question',
-    slug: Slug.create('example-question'),
-    content: 'example content',
-    ...override,
-  })
+export function makeQuestion(
+  override: Partial<QuestionProps> = {},
+  id?: UniqueEntityID,
+) {
+  const question = Question.create(
+    {
+      authorId: new UniqueEntityID(),
+      title: faker.lorem.sentence(),
+      content: faker.lorem.text(),
+      ...override,
+    },
+    id,
+  )
 
   return question
 }
