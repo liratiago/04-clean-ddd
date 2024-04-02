c4916a42124e01bae99adf6391ad03df2d1cae4a
Tiago <tiagoufcg@gmail.com>
2024-04-02 08:36:32 -0300

    comentarios nas respostas

---
+++ src/domain/forum/application/use-cases/comment-on-answer.ts
@@ -0,0 +1,45 @@
+import { AnswerComment } from '../../enterprise/entities/answer-coment'
+import { AnswerCommentsRepository } from '../repositories/answer-comments-repository'
+import { AnswersRepository } from '../repositories/answers-repository'
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+
+interface ComentOnAnswerUseCaseRequest {
+  authorId: string
+  answerId: string
+  content: string
+}
+
+interface ComentOnAnswerUseCaseResponse {
+  answerComment: AnswerComment
+}
+
+export class CommentOnQuestionUseCase {
+  constructor(
+    private ansewrsRepository: AnswersRepository,
+    private answerCommentsRepository: AnswerCommentsRepository,
+  ) {}
+
+  async execute({
+    authorId,
+    answerId,
+    content,
+  }: ComentOnAnswerUseCaseRequest): Promise<ComentOnAnswerUseCaseResponse> {
+    const answer = this.ansewrsRepository.findById(answerId)
+
+    if (!answer) {
+      throw new Error('Answer not found')
+    }
+
+    const answerComment = AnswerComment.create({
+      authorId: new UniqueEntityID(authorId),
+      answerId: new UniqueEntityID(answerId),
+      content,
+    })
+
+    await this.answerCommentsRepository.create(answerComment)
+
+    return {
+      answerComment,
+    }
+  }
+}
