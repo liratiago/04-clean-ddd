b889477d70b54c42e0a41f0ec3c830be653cc100
Tiago <tiagoufcg@gmail.com>
2024-04-02 09:48:49 -0300

    listar comentarios de respostas

--- test/repositories/in-memory-answer-comments-repository.ts
+++ test/repositories/in-memory-answer-comments-repository.ts
@@ -1,3 +1,4 @@
+import { PaginationParams } from '@/core/repositories/pagination-params'
 import { AnswerCommentsRepository } from '@/domain/forum/application/repositories/answer-comments-repository'
 import { AnswerComment } from '@/domain/forum/enterprise/entities/answer-coment'
 
@@ -26,5 +27,12 @@ export class InMemoryAnswerCommentsRepository
     )
 
     this.items.splice(itemIndex, 1)
+  }
+
+  async findManyByAnswerId(answerId: string, { page }: PaginationParams) {
+    const answerComments = this.items
+      .filter((item) => item.answerId.toString() === answerId)
+      .slice((page - 1) * 20, page * 20)
+    return answerComments
   }
 }
